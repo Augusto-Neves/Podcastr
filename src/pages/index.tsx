cbcf763ab949a4f03522dbaaff3a5787954edc18
Augusto Neves <augusto.agt1995@gmail.com>
2021-04-25 01:00:56 -0300

    Funcionalidade de Play e Pause

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
 
+import { PlayerContext } from '../context/PlayerContext';
 import { api } from '../services/api'
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -26,6 +28,7 @@ type HomeProps = {
 }
 
 export default function Home({ lastedEpisodes, allEpisodes }: HomeProps) {
+  const { play } =  useContext(PlayerContext);
 
   return (
     <div className={styles.homePage}>
@@ -52,7 +55,7 @@ export default function Home({ lastedEpisodes, allEpisodes }: HomeProps) {
                   <span> {episode.durationAsString} </span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={()=> play(episode)}>
                   <img src="/play-green.svg" alt="Reproduzir" />
                 </button>
               </li>
